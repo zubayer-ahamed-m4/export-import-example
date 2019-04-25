a89d7cc5c1fb280d85a725af78966c43b262b77f
zubay <zubay@192.168.0.68>
2019-04-25 13:21:40 +0600

    1st commit

---
+++ src/main/java/com/coderslab/ExportImportExampleApplication.java
@@ -0,0 +1,13 @@
+package com.coderslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportImportExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportImportExampleApplication.class, args);
+	}
+
+}
