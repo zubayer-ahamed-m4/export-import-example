122551e802180af83143127c3b0af4280016951e
zubay <zubay@DESKTOP-FR0HQC8>
2019-04-30 09:15:39 +0600

    hi

--- src/test/java/com/coderslab/SpreedSheetColumnGenerate.java
+++ src/test/java/com/coderslab/SpreedSheetColumnGenerate.java
@@ -4,13 +4,20 @@ import org.junit.Test;
 
 public class SpreedSheetColumnGenerate {
 
-	private String[] columns = new String[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-	
+	private String[] columns = new String[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+			"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+	private int INTERVAL = 0;
+	private int MAX_INTERVAL = 5;
+	private String PREFIX = "";
+
 	@Test
 	public void test() {
-		int interval = 2;
-		for(int i = 0; i < columns.length; i++) {
-			System.out.println(columns[i]);
+		for (int i = 0; i < columns.length; i++) {
+			if (i == 0) INTERVAL++;
+			if (INTERVAL > MAX_INTERVAL) break;
+			if (INTERVAL > 1 && i == 0) PREFIX = columns[INTERVAL - 1];
+			System.out.println(PREFIX + columns[i]);
+			if (i == columns.length - 1) i = -1;
 		}
 	}
 }
